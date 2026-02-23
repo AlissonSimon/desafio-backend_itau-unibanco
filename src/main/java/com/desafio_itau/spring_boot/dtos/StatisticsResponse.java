a844cb3141edcc8d98b83ce22919c6610dde71ee
Alisson Simon <alissonsimon21@gmail.com>
2026-02-23 14:28:37 -0300

    feat: creating StatisticsResponse

---
+++ src/main/java/com/desafio_itau/spring_boot/dtos/StatisticsResponse.java
@@ -0,0 +1,22 @@
+package com.desafio_itau.spring_boot.dtos;
+
+import lombok.Getter;
+
+import java.util.DoubleSummaryStatistics;
+
+@Getter
+public class StatisticsResponse {
+    private long count;
+    private double sum;
+    private double avg;
+    private double min;
+    private double max;
+
+    public StatisticsResponse(DoubleSummaryStatistics stats) {
+        this.count = stats.getCount();
+        this.sum = stats.getSum();
+        this.avg = stats.getAverage();
+        this.min = stats.getMin();
+        this.max = stats.getMax();
+    }
+}
